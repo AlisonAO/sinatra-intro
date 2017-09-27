eb72d817143596c3f5a5ec774382049d42008dc9
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-27 10:15:24 +0100

    Added extra notes

--- app.rb
+++ app.rb
@@ -2,31 +2,32 @@ require 'sinatra'
 require_relative "./TodoManager.rb"
 
 get '/todos' do
+	#INDEX - all th data
 	TodoManager.index
 end
 
 post '/todos' do
-	# "CREATE"
+	# "CREATE" - create a new todo
 	text = params[:text]
 	TodoManager.create(text)
 end
 
 get '/todos/:id' do
 	id = params[:id]
-	# "SHOW" #{id}
+	# "SHOW" #{id} - show a single record. retrieves it from the specific id
 	TodoManager.show(id.to_i)
 
 end
 
 put '/todos/:id' do
-	id = params[:id]
+	id = params[:id] 
 	text = params[:text]
-	# "UPDATE" #{id}
+	# "UPDATE" #{id} - update a single record
 	TodoManager.update(id.to_i, text)
 end
 
 delete '/todos/:id' do
 	id = params[:id] #dynamic url attribute
-	# "DESTROY" #{id}
+	# "DESTROY" #{id} - deletes a single record
 	TodoManager.delete(id.to_i)
 end
